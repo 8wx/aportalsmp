8b208f7f649320b709418668af5ffc443b41dcfe
Archer <84819659+8wx@users.noreply.github.com>
2025-11-06 20:05:32 +0200

    Add setup.py for pip installation from GitHub

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aportalsmp",
+    version="1.3",
+    description="Asynchronous API wrapper for Portals Marketplace (fork with dynamic collection mapping)",
+    author="bleach (original), 8wx (fork)",
+    url="https://github.com/8wx/aportalsmp",
+    packages=find_packages(),
+    install_requires=[
+        "curl-cffi",
+        "kurigram"
+    ],
+    python_requires=">=3.10",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: OS Independent",
+    ],
+)
